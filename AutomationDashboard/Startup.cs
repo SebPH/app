2a2d4554aacf7f1661bb60177899231e5a24851e
Sebastian Puerta Hincapie <sebastian.ph@incedoinc.com>
2019-12-26 17:11:03 -0500

    saving initial configuration

---
+++ AutomationDashboard/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutomationDashboard
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                Console.WriteLine("Development Environment");
+                app.UseDeveloperExceptionPage();
+            }
+            else if (env.IsStaging())
+            {
+                Console.WriteLine("Staging Environment");
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            }
+            else if (env.IsProduction())
+            {
+                Console.WriteLine("Production Environment");
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            }
+            else if (env.IsEnvironment("Test"))
+            {
+                Console.WriteLine("Test Environment");
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
